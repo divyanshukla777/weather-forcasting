7f69b0f3ccec68a7ade07f7414ca69b1bba49b27
Divyanshu Shukla <divyanshukla777@gmail.com>
2020-06-19 11:20:03 +0530

    g

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  console.log('iiiiiiii',gaID);
+  console.log(`iiiiiiii....${gaID}`);
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again',gaID:gaID});
