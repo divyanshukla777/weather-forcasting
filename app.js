00274416b96419a3435a252397623b4e3eaa9fd5
Divyanshu Shukla <divyanshukla777@gmail.com>
2020-06-19 11:50:07 +0530

    ga

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.post('/', function (req, res) {
   logger.info(`iiiiiiii....${gaID}`);
   request(url, function (err, response, body) {
     if(err){
-      res.render('index', {weather: null, error: 'Error, please try again',gaID:gaID});
+      res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again',gaID:gaID});
+        res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${+((weather.main.temp-32)*(5/9)).toFixed(3)} degree celcius in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null,gaID:gaID});
+        res.render('index', {weather: weatherText, error: null});
       }
     }
   });
