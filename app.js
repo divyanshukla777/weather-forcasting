635ce2711f16a5bbbff8a65bb35a0afcb2ee1113
Divyanshu Shukla <divyanshukla777@gmail.com>
2020-06-19 11:39:25 +0530

    l

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const logger=require('heroku-logger');
 const app = express()
 require('dotenv').config()
 
@@ -18,7 +19,7 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  console.log(`iiiiiiii....${gaID}`);
+  logger.info(`iiiiiiii....${gaID}`);
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again',gaID:gaID});
